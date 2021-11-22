6fa323dc4d6a8f64c251da2f5e6476f2a1fc453d
MayThanHsuAung <maythanhsuaung1111@gmail.com>
2021-11-22 17:43:11 +0630

    'update-databaseConfig'

---
+++ config/env/production/database.js
@@ -0,0 +1,25 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(parse('postgres://rihhmvjwsoydot:597d6633d7f5170cada93f3b0b5a46d2702782ef8db0dc9086c85c722e0e8988@ec2-44-198-146-224.compute-1.amazonaws.com:5432/d7gpe425f209qi'));
+
+module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'bookshelf',
+            settings: {
+                client: 'postgres',
+                host: config.host,
+                port: config.port,
+                database: config.database,
+                username: config.user,
+                password: config.password,
+                ssl: {
+                    rejectUnauthorized: false,
+                },
+            },
+            options: {
+                ssl: true,
+            },
+        },
+    },
+});

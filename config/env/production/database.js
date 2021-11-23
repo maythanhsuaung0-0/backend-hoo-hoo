a20f16639ddde51771111a88573f1298371a1598
MayThanHsuAung <maythanhsuaung1111@gmail.com>
2021-11-23 16:01:05 +0630

    fix env/prod

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,5 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(parse('postgres://rihhmvjwsoydot:597d6633d7f5170cada93f3b0b5a46d2702782ef8db0dc9086c85c722e0e8988@ec2-44-198-146-224.compute-1.amazonaws.com:5432/d7gpe425f209qi'));
+const config = parse('postgres://rihhmvjwsoydot:597d6633d7f5170cada93f3b0b5a46d2702782ef8db0dc9086c85c722e0e8988@ec2-44-198-146-224.compute-1.amazonaws.com:5432/d7gpe425f209qi');
 
 module.exports = ({ env }) => ({
     defaultConnection: 'default',
